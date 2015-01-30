22277b00927e6afe2edee6812edb70ff1cfeb50e
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-01-30 11:35:05 +0530

    [Intents] Allow intent flags.

--- extensions/src/main/kotlin/com/mobsandgeeks/ake/Intents.kt
+++ extensions/src/main/kotlin/com/mobsandgeeks/ake/Intents.kt
@@ -6,45 +6,97 @@ import android.app.Activity
 import android.app.Service
 import android.os.Bundle
 
+public fun Context.flags(flag: Int, vararg flags: Int): Int {
+    var orFlags = flag;
+    for (i in flags) {
+        orFlags = orFlags.or(i)
+    }
+    return orFlags
+}
 
 inline public fun <reified T : Activity> Activity.startActivity() {
     this.startActivity(getIntent<T>())
+}
+
+inline public fun <reified T : Activity> Activity.startActivity(flags: Int) {
+    this.startActivity(getIntent<T>(flags))
 }
 
 inline public fun <reified T: Activity> Activity.startActivity(extras: Bundle) {
     this.startActivity(getIntent<T>(extras))
 }
 
+inline public fun <reified T: Activity> Activity.startActivity(extras: Bundle, flags: Int) {
+    this.startActivity(getIntent<T>(extras, flags))
+}
+
 inline public fun <reified T : Activity> Activity.startActivityForResult(requestCode: Int) {
     this.startActivityForResult(getIntent<T>(), requestCode)
 }
 
+inline public fun <reified T : Activity> Activity.startActivityForResult(
+        requestCode: Int, flags: Int) {
+    this.startActivityForResult(getIntent<T>(flags), requestCode)
+}
+
+inline public fun <reified T : Activity> Activity.startActivityForResult(
+        extras: Bundle, requestCode: Int) {
+    this.startActivityForResult(getIntent<T>(extras), requestCode)
+}
+
+inline public fun <reified T : Activity> Activity.startActivityForResult(
+        extras: Bundle, requestCode: Int, flags: Int) {
+    this.startActivityForResult(getIntent<T>(extras, flags), requestCode)
+}
+
 inline public fun <reified T : Activity> Service.startActivity() {
-    val intent = getIntent<T>()
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    this.startActivity(intent)
+    this.startActivity(getIntent<T>(Intent.FLAG_ACTIVITY_NEW_TASK))
+}
+
+inline public fun <reified T : Activity> Service.startActivity(flags: Int) {
+    this.startActivity(getIntent<T>(flags))
 }
 
 inline public fun <reified T: Activity> Service.startActivity(extras: Bundle) {
-    val intent = getIntent<T>(extras);
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    this.startActivity(intent)
+    this.startActivity(getIntent<T>(extras, Intent.FLAG_ACTIVITY_NEW_TASK))
+}
+
+inline public fun <reified T: Activity> Service.startActivity(extras: Bundle, flags: Int) {
+    this.startActivity(getIntent<T>(extras, flags))
 }
 
 inline public fun <reified T: Service> Context.startService() {
     this.startService(getIntent<T>())
 }
 
+inline public fun <reified T: Service> Context.startService(flags: Int) {
+    this.startService(getIntent<T>(flags))
+}
+
 inline public fun <reified T: Service> Context.startService(extras: Bundle) {
     this.startService(getIntent<T>(extras))
+}
+
+inline public fun <reified T: Service> Context.startService(extras: Bundle, flags: Int) {
+    this.startService(getIntent<T>(extras, flags))
 }
 
 inline public fun <reified T: Context> Context.getIntent(): Intent {
     return Intent(this, javaClass<T>())
 }
 
+inline public fun <reified T: Context> Context.getIntent(flags: Int): Intent {
+    val intent = getIntent<T>()
+    intent.setFlags(flags)
+    return intent
+}
+
 inline public fun <reified T: Context> Context.getIntent(extras: Bundle): Intent {
-    val intent = Intent(this, javaClass<T>())
+    return getIntent<T>(extras, 0)
+}
+
+inline public fun <reified T: Context> Context.getIntent(extras: Bundle, flags: Int): Intent {
+    val intent = getIntent<T>(flags)
     intent.putExtras(extras)
     return intent
 }
