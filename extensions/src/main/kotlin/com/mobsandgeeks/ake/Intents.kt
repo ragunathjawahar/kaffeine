14491bcca733a1dbb882b97e8e5932e8cdd05eb3
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-01-30 08:07:26 +0530

    [Intents] Start activities and services.

--- extensions/src/main/kotlin/com/mobsandgeeks/ake/Intents.kt
+++ extensions/src/main/kotlin/com/mobsandgeeks/ake/Intents.kt
@@ -4,12 +4,47 @@ import android.content.Context
 import android.content.Intent
 import android.app.Activity
 import android.app.Service
+import android.os.Bundle
 
 
-public fun <T : Activity> Context.startActivity(activityClass: Class<T>) {
-    this.startActivity(Intent(this, activityClass))
+inline public fun <reified T : Activity> Activity.startActivity() {
+    this.startActivity(getIntent<T>())
 }
 
-public fun <T: Service> Context.startService(serviceClass: Class<T>) {
-    this.startService(Intent(this, serviceClass))
+inline public fun <reified T: Activity> Activity.startActivity(extras: Bundle) {
+    this.startActivity(getIntent<T>(extras))
+}
+
+inline public fun <reified T : Activity> Activity.startActivityForResult(requestCode: Int) {
+    this.startActivityForResult(getIntent<T>(), requestCode)
+}
+
+inline public fun <reified T : Activity> Service.startActivity() {
+    val intent = getIntent<T>()
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    this.startActivity(intent)
+}
+
+inline public fun <reified T: Activity> Service.startActivity(extras: Bundle) {
+    val intent = getIntent<T>(extras);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    this.startActivity(intent)
+}
+
+inline public fun <reified T: Service> Context.startService() {
+    this.startService(getIntent<T>())
+}
+
+inline public fun <reified T: Service> Context.startService(extras: Bundle) {
+    this.startService(getIntent<T>(extras))
+}
+
+inline public fun <reified T: Context> Context.getIntent(): Intent {
+    return Intent(this, javaClass<T>())
+}
+
+inline public fun <reified T: Context> Context.getIntent(extras: Bundle): Intent {
+    val intent = Intent(this, javaClass<T>())
+    intent.putExtras(extras)
+    return intent
 }
