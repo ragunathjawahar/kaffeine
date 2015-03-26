ae4600204e71b586a9fa3117b485f798c8ac3c65
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-03-27 01:47:00 +0530

    [Platform] Add platform version functions.

---
+++ kaffeine/src/main/kotlin/com/mobsandgeeks/kaffeine/Platform.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015 Mobs & Geeks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mobsandgeeks.kaffeine
+
+import android.os.Build
+
+
+public fun Any.platformVersion(): Int = Build.VERSION.SDK_INT
+
+public fun Any.platformIsOlderThan(version: Int): Boolean = Build.VERSION.SDK_INT < version
+
+public fun Any.platformHasApisFrom(version: Int): Boolean = Build.VERSION.SDK_INT >= version
